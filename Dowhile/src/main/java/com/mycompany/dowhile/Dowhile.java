680924840ac9277e307a7f2d810244bd3a0c6dbe
Shirley Campos <125100748+Shirleyc11@users.noreply.github.com>
2024-09-27 22:08:23 -0300

    While
     Run ilimitado:
    Do While = Limitado
    While= Ilimitado

---
+++ Dowhile/src/main/java/com/mycompany/dowhile/Dowhile.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dowhile;
+
+/**
+ *
+ * @author Admin
+ */
+public class Dowhile {
+
+    public static void main(String[] args) {
+        int i =1;
+        do{
+                System.out.println("i="+i);
+        }while(i!=1);
+    }
+}
---
+++ WhileProject/src/main/java/com/mycompany/whileproject/WhileProject.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.whileproject;
+
+/**
+ *
+ * @author Admin
+ */
+public class WhileProject {
+
+    public static void main(String[] args) {
+        int i =2;
+        while(i!=1){
+            System.out.println("i="+i++);
+        }
+    }
+}
